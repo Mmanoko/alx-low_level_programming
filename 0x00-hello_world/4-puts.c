d1c50af25dcf8fae4dfe127d1e444cb96372b8c4
Mmanoko <mohlabapm68@gmail.com>
2023-07-13 08:49:55 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+julien@ubuntu:~/c/0x00$ cat main.c
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+  * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
