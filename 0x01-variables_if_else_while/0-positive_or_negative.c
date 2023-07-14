bbdd93cb5b1617d786f944585ea8767f8898e85f
Mmanoko <mohlabapm68@gmail.com>
2023-07-14 08:43:16 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,18 +12,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/*   */
-	 if (n < 0)
-	 {
-		 printf("%d is %s\n", n, "negative");
-	 }
-	 else if(n > 0)
-	 {
-		 printf("%d is %s\n", n, "positive");
-	 }
-	 else
-	 {
+	/*  program */
+	if (n < 0)
+	{
+		printf("%d is %s\n", n, "negative");
+	}
+	else if (n == 0)
+	{
 		 printf("%d is %s\n", n, "zero");
-	 }
-	 return (0);
+	}
+	else
+	{
+		 printf("%d is %s\n", n, "positive");
+	}
+	return (0);
 }
