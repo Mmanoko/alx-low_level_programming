37150a13180423421fdbc3173093b82daa553784
Mmanoko <mohlabapm68@gmail.com>
2023-07-20 20:44:52 -0700

    main.h

---
+++ 0x01-variables_if_else_while/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+3-print_alphabets.c;
+4-print_alphabt.c;
+5-print_numbers.c;
+6-print_numberz.c;
+7-print_tebahpla.c;
+8-print_base16.c;
+9-print_comb.c;
+100-print_comb3.c;
+101-print_comb4.c;
+102-print_comb5.c
+
+#endif
